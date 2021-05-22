14c201874e1cf9c1371e4576296c95dfeb9e33fd
Dave Kitzmiller <d.kitzmiller@gmail.com>
2021-05-21 18:43:24 -0700

    init

---
+++ src/components/card.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Card = () => {
+    return <h2>movie #1</h2>;
+};
+export default Card;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './containers/List.js';
+
+const App = () => {
+    return <List />
+};
+ReactDOM.render(<App/>, document.getElementById('root'));
